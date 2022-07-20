c5e13a5d46481b655739c3dce0894ecd78e275e2
rpham64 <rpham64@gmail.com>
2022-07-20 08:48:17 -0700

    Set up UI layer components

--- app/src/main/java/com/example/squaretake_homeproject/ui/EmployeeDirectoryActivity.kt
+++ app/src/main/java/com/example/squaretake_homeproject/ui/EmployeeDirectoryActivity.kt
@@ -2,6 +2,7 @@ package com.example.squaretake_homeproject.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.squaretake_homeproject.EmployeeDirectoryApplication
 import com.example.squaretake_homeproject.R
 import javax.inject.Inject
 
@@ -10,7 +11,30 @@ class EmployeeDirectoryActivity : AppCompatActivity() {
     @Inject lateinit var employeeDirectoryViewModel: EmployeeDirectoryViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        // Inject app components
+        (applicationContext as EmployeeDirectoryApplication).appComponent.inject(this)
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_employee_directory)
+
+        employeeDirectoryViewModel.results.observe(this) { result ->
+            when {
+                result.isSuccess -> {
+
+                }
+                result.isFailure -> {
+
+                }
+            }
+        }
+        employeeDirectoryViewModel.fetchEmployeesList()
+    }
+
+    private fun showError() {
+
+    }
+
+    private fun showEmpty() {
+
     }
 }
--- app/src/main/java/com/example/squaretake_homeproject/ui/EmployeeDirectoryViewModel.kt
+++ app/src/main/java/com/example/squaretake_homeproject/ui/EmployeeDirectoryViewModel.kt
@@ -1,10 +1,27 @@
 package com.example.squaretake_homeproject.ui
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.squaretake_homeproject.data.EmployeeDirectoryRepository
+import com.example.squaretake_homeproject.data.model.Employee
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
-class EmployeeDirectoryViewModel : ViewModel() {
+class EmployeeDirectoryViewModel @Inject constructor(
+    private val employeeDirectoryRepository: EmployeeDirectoryRepository
+) : ViewModel() {
 
-    @Inject lateinit var employeeDirectoryRepository: EmployeeDirectoryRepository
+    private var _results: MutableLiveData<Result<List<Employee>>> = MutableLiveData(Result.success(emptyList()))
+    val results: LiveData<Result<List<Employee>>> = _results
+
+    fun fetchEmployeesList() {
+        viewModelScope.launch(Dispatchers.IO) {
+            val result = employeeDirectoryRepository.getEmployeesList()
+
+            _results.postValue(result)
+        }
+    }
 }

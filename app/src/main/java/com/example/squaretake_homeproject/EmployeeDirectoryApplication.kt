db1af9f2f2cb93b9d6ba3d0b7573b1d85144a590
rpham64 <rpham64@gmail.com>
2022-07-20 10:44:09 -0700

    Fixed unresolved reference in dagger component

--- app/src/main/java/com/example/squaretake_homeproject/EmployeeDirectoryApplication.kt
+++ app/src/main/java/com/example/squaretake_homeproject/EmployeeDirectoryApplication.kt
@@ -2,8 +2,9 @@ package com.example.squaretake_homeproject
 
 import android.app.Application
 import com.example.squaretake_homeproject.di.DaggerEmployeeDirectoryComponent
+import com.example.squaretake_homeproject.di.EmployeeDirectoryComponent
 
 class EmployeeDirectoryApplication : Application() {
 
-    val appComponent = DaggerEmployeeDirectoryComponent.create()
+    val appComponent: EmployeeDirectoryComponent = DaggerEmployeeDirectoryComponent.create()
 }
